d2cb9a53bd27e49c62aa99e5e8d73dd58daa15c8
srbadev <srbadev@gmail.com>
2024-03-21 12:39:06 +0100

    initial commit

---
+++ src/main/java/com/keneath/spring_keycloak_saml_identity_provider/SpringKeycloakSamlIdentityProviderApplication.java
@@ -0,0 +1,13 @@
+package com.keneath.spring_keycloak_saml_identity_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKeycloakSamlIdentityProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKeycloakSamlIdentityProviderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keneath/spring_keycloak_saml_identity_provider/SpringKeycloakSamlIdentityProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keneath.spring_keycloak_saml_identity_provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKeycloakSamlIdentityProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
